2ea79443adc4c8d0967c32245f61fbc93edf700a
徳永直哉 <tokunaganaoya@tokunaganaoyanoMacBook-Pro.local>
2025-03-27 15:04:45 +0900

    エラー画面の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,22 @@
 
 class ApplicationController < ActionController::Base
   before_action :require_login
+  rescue_from StandardError, with: :render500
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
+
+  def render500(error = nil)
+    Rails.logger.error("❌#{error.message}") if error
+    render template: 'errors/error500', layout: 'error', status: :internal_server_error
+  end
+
+  def render404(error = nil)
+    Rails.logger.error("❌#{error.message}") if error
+    render template: 'errors/error404', layout: 'error', status: :not_found
+  end
+
+  def raise500
+    raise '意図的な500エラー'
+  end
 
   private
 
